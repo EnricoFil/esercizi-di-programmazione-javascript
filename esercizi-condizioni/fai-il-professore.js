9db56bbb449754c274edd559609f2e4cc330b094
EnricoFil <evol_enrico@hotmail.it>
2025-08-29 18:33:43 +0200

    Update fai-il-professore.js

--- esercizi-condizioni/fai-il-professore.js
+++ esercizi-condizioni/fai-il-professore.js
@@ -18,3 +18,53 @@
 
   http://www.imparareaprogrammare.it
 */
+//Esercizio svolto con if ed else//
+var v = 23;
+console.log("voto = "+ v);
+if (v < 18) {
+  console.log("Insufficiente");
+} else if (v < 21) {
+  // qui sappiamo che v è almeno 18, quindi 18 <= v < 21
+  console.log("Sufficiente");
+} else if (v < 24) {
+  console.log("Buono");
+} else if (v < 27) {
+  console.log("Distinto");
+} else if (v <= 29) {
+  console.log("Ottimo");
+} else if (v === 30) {
+  console.log("Eccellente");
+} else {
+  console.log("Voto non valido");
+}
+
+//esercizio svolto con switch case//
+
+var v = 23;
+console.log("voto = "+ v);
+switch (true) {
+  case (v < 18):
+console.log("Insufficiente");
+
+    break;
+  case (v < 21):
+  console.log("Sufficiente");
+
+    break;
+    case (v < 24):
+      console.log("Buono");
+
+  break;
+case (v < 27):
+  console.log("Distinto");
+
+  break;
+  case (v <= 29):
+    console.log("Ottimo");
+    break;
+  case (v == 30):
+    console.log("Eccellente");
+    break;
+default:
+console.log("Voto non valido");
+}
