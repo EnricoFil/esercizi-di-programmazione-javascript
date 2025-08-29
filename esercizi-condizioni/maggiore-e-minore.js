e805e9632a25269b52b0fefde99bbe87c203f3ee
EnricoFil <evol_enrico@hotmail.it>
2025-08-29 17:52:06 +0200

    Update maggiore-e-minore.js

--- esercizi-condizioni/maggiore-e-minore.js
+++ esercizi-condizioni/maggiore-e-minore.js
@@ -10,3 +10,28 @@
 
   http://www.imparareaprogrammare.it
 */
+var a = 5;
+var b = -7;
+var c = 31;
+var d = 43;
+
+var maggiore = a;
+var minore = a;
+
+if (b>maggiore) { maggiore = b;
+
+}if (c>maggiore) {maggiore = c;
+
+}if (d>maggiore) {maggiore = d;
+
+}
+if (b<minore) {minore = b;
+
+}if (c<minore) {minore = c;
+
+}if (d<minore) {minore = d;
+
+}
+
+console.log("maggiore = " + maggiore);
+console.log("minore = " + minore);
