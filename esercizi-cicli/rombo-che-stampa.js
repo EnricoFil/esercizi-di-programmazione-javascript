50610b4de37d2ac215b2266341c1d77a42c6dd53
EnricoFil <evol_enrico@hotmail.it>
2025-09-08 17:49:30 +0200

    Update rombo-che-stampa.js

--- esercizi-cicli/rombo-che-stampa.js
+++ esercizi-cicli/rombo-che-stampa.js
@@ -16,3 +16,42 @@
   
   http://www.imparareaprogrammare.it
 */
+var n = 5;
+
+
+for (var i = 1; i <= Math.ceil(n / 2); i++) {
+  var numX = 2 * i - 1;
+  var numSpazi = (n - numX) / 2;
+
+  var linea = "";
+
+
+  for (var s = 0; s < numSpazi; s++) {
+    linea += " ";
+  }
+
+  for (var x = 0; x < numX; x++) {
+    linea += "X";
+  }
+
+  console.log(linea);
+}
+
+for (var i = Math.floor(n / 2); i >= 1; i--) {
+  var numX = 2 * i - 1;
+  var numSpazi = (n - numX) / 2;
+
+  var linea = "";
+  for (var s = 0; s < numSpazi; s++) {
+   linea += " ";
+ }
+
+ for (var x = 0; x < numX; x++) {
+   linea += "X";
+ }
+
+ console.log(linea);
+}
+
+
+\\questo veramente terribile btw\\
